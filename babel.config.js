e753b7d0cc336c17e1a539710f158c9bb95515a5
LAPTOP-GMIOJVE5\jxx <986115849@qq.com>
2022-07-15 14:05:36 +0800

    chore(src/*): 组件初始化

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,20 @@
+/*
+ * @Descripttion:
+ * @Version:
+ * @Author: jiaoxingxing
+ * @Date: 2022-06-14 17:11:07
+ * @LastEditors: jiaoxingxing
+ * @LastEditTime: 2022-07-15 14:01:41
+ */
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
-    ['@babel/preset-env', {
-      useBuiltIns: 'entry', // or "usage"
-      corejs: 3
-    }]
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry', // or "usage"
+        corejs: 3
+      }
+    ]
   ]
 }
